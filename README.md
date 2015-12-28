13d084f998c29ab4989fa769f72f849e3e1c0065
Dmytro Sokil <dmytro.sokil@gmail.com>
2015-12-28 23:40:53 +0200

    tests

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# url.js
+# Url.js
 
 Library to work with urls
+
+## Tests
+
+You must install `jasmine`:
+```
+npm install -g jasmine
+```
+
+Then run in console:
+```
+cd tests
+jasmine
+```
---
+++ tests/spec/UrlSpec.js
@@ -0,0 +1,67 @@
+describe("Url suite", function() {
+    var Url = require('../../src/Url.js');
+
+    it("converts 'param=value' to object", function() {
+        expect(
+            {"param": "value"}
+        ).toEqual(
+            Url.stringToObject('param=value')
+        );
+    });
+
+    it("converts 'param[]=value' to object", function() {
+        expect(
+            {"param": {"0": "value"}}
+        ).toEqual(
+            Url.stringToObject('param[]=value')
+        );
+    });
+
+    it("converts 'param[]=value1&param[]=value2' to object", function() {
+        expect(
+            {"param": {"0": "value1", "1": "value2"}}
+        ).toEqual(
+            Url.stringToObject('param[]=value1&param[]=value2')
+        );
+    });
+
+    it("converts 'param[key]=value' to object", function() {
+        expect(
+            {"param": {"key": "value"}}
+        ).toEqual(
+            Url.stringToObject('param[key]=value')
+        );
+    });
+
+    it("converts 'param[key1]=value1&param[key2]=value2' to object", function() {
+        expect(
+            {"param": {"key1": "value1", "key2": "value2"}}
+        ).toEqual(
+            Url.stringToObject('param[key1]=value1&param[key2]=value2')
+        );
+    });
+
+    it("converts encoded value with spaces to object", function() {
+        expect(
+            {"param": "Зроблено в Україні"}
+        ).toEqual(
+            Url.stringToObject('param=%D0%97%D1%80%D0%BE%D0%B1%D0%BB%D0%B5%D0%BD%D0%BE%20%D0%B2%20%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D1%96')
+        );
+    });
+
+    it("converts value with plus sign to object", function() {
+        expect(
+            {"param": "1 1"}
+        ).toEqual(
+            Url.stringToObject('param=1+1')
+        );
+    });
+
+    it("converts value with encoded plus sign to object", function() {
+        expect(
+            {"param": "1+1"}
+        ).toEqual(
+            Url.stringToObject('param=1%2B1')
+        );
+    });
+});
